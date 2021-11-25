58342526044615bb9a742e9755eccf76cf870988
Ronny Solheim <rs@duett.no>
2021-11-25 14:45:18 +0100

    More clients

---
+++ Examples/Python/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.article_api import ArticleApi
+from swagger_client.api.carrier_api import CarrierApi
+from swagger_client.api.carrier_type_api import CarrierTypeApi
+from swagger_client.api.customer_api import CustomerApi
+from swagger_client.api.hour_registration_api import HourRegistrationApi
+from swagger_client.api.hour_registration_document_api import HourRegistrationDocumentApi
+from swagger_client.api.project_api import ProjectApi
